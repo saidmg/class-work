5e81df18bdc5c8214a0ddb86573378be182fd801
Said Mg <saidmghabghab@gmail.com>
2021-02-27 12:41:35 -0500

    Changed to arrow function

--- app/orm.js
+++ app/orm.js
@@ -2,12 +2,12 @@ const { response } = require('express')
 
 const db = require('./connection')('contact_book', 'skotkalb')
 
-async function getInfo1(){
+const getInfo1 =async () => {
    let result =  db.query(`SELECT * from contact Where id = ${id}`)
    return result
 }
 
-async function searchFunction(contactInfo){
+const searchFunction = async(contactInfo) => {
     let result
     if ( contactInfo.length>1){
         result  = await db.query(`SELECT * from contact Where first_name LIKE '%${contactInfo[0]}%' AND last_name LIKE '%${contactInfo[1]}%'`)
@@ -19,22 +19,22 @@ async function searchFunction(contactInfo){
     return result
 }
 
-async function createContactFunction(contactInfo){
+const createContactFunction = async (contactInfo) => {
     const result = await db.query(`INSERT INTO contact (first_name,last_name,phone_number) VALUES
     (?,?,?)`,[contactInfo.firstName,contactInfo.lastName,contactInfo.phoneNumber])
     return result
 }
 
-async function deleteContactFunction(deleteId){
+const deleteContactFunction = async (deleteId) => {
     let result = await db.query(`DELETE from contact Where id =?`, deleteId)
 return result
 }
-async function updateContactFunction(id, contactInfo){
+const updateContactFunction = async (id, contactInfo) => {
     let result = await db.query(`UPDATE contact SET first_name ='${contactInfo[0].first_name}' , last_name= '${contactInfo[0].last_name}' , phone_number ='${contactInfo[0].phone_number}' WHERE id = ${id} `);
      return(result)
 }
 
-async function showContactFunction(){
+const showContactFunction = async () => {
     let result = await db.query('SELECT * from contact');
     return result
 }
